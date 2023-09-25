218f7d8bdcf3b79d395c8c8ff845a9d9f89e4952
SDev7722.Zinskyi Stanislav <stas.dolon@gmail.com>
2023-09-25 20:53:43 +0300

    first pages

--- src/route/index.js
+++ src/route/index.js
@@ -1,22 +1,37 @@
 // Підключаємо технологію express для back-end сервера
-const express = require('express')
+const express = require("express");
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
-const router = express.Router()
+const router = express.Router();
 
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
+router.get("/", function (req, res) {
+	// res.render генерує нам HTML сторінку
 
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
-  //                  ↑↑ сюди вводимо JSON дані
-})
+	//            ↙ cюди вводимо назву файлу з сontainer
+	res.render("index", {});
+	//                  ↑↑ сюди вводимо JSON дані
+});
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get("/selector", function (req, res) {
+	// res.render генерує нам HTML сторінку
+
+	//            ↙ cюди вводимо назву файлу з сontainer
+	res.render("selector", {
+		layout: "selector",
+	});
+	//                  ↑↑ сюди вводимо JSON дані
+});
 
 // ================================================================
 
 // Підключаємо роутер до бек-енду
-module.exports = router
+module.exports = router;
